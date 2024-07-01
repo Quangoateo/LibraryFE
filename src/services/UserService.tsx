bb0362412e5d66bf23a79fee5053f5bd6d567b3c
NgocQuang <64728000+Quangoateo@users.noreply.github.com>
2024-07-01 21:38:26 +0700

    add libfe

--- src/services/UserService.tsx
+++ src/services/UserService.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const REST_API_BASE_URL = 'https://proactive-elegance-production.up.railway.appp/api/readersinfo';
+const REST_API_BASE_URL = 'https://proactive-elegance-production.up.railway.app/api/readersinfo';
 
 
 
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
+  build: {
+    outDir: 'build',
+  },
+
   server: {
     port: 3000,
   }
